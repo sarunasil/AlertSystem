7c0235f3fc23b1abe839bed97ed70207fb4ac9e5
Nikolay Stanchev <ns17@it-innovation.soton.ac.uk>
2019-05-12 17:52:56 +0100

    Updates cloud app and cloud visualiser to use IPv6

--- cloud_application/visualiser.py
+++ cloud_application/visualiser.py
@@ -131,4 +131,4 @@ def update_graph():
 app.layout = update_graph
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=False)
+    app.run_server(host='::', port=8050, debug=False)
